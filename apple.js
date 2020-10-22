b2e03003b07410b528a39712e4ce49426d8f59fe
solheee <09merus@gmail.com>
2020-10-22 21:08:26 +0900

    커밋커밋을하자커어밋

---
+++ apple.js
@@ -0,0 +1,29 @@
+import { onSnake, expandSnake } from './snake.js';
+import { randomGridPosition } from './grid.js';
+
+let apple = getRandomApplePosition();
+const EXPANSION_RATE = 1;
+
+export function update() {
+    if (onSnake(apple)) {
+        expandSnake(EXPANSION_RATE);
+        apple = getRandomApplePosition();
+    }
+};
+
+export function draw(gameBoard) {
+    const appleElement = document.createElement('div');
+    appleElement.style.gridRowStart = apple.y;
+    appleElement.style.gridColumnStart = apple.x;
+    appleElement.classList.add('apple');
+    gameBoard.appendChild(appleElement);
+
+};
+
+function getRandomApplePosition() {
+    let newApplePosition
+    while (newApplePosition == null || onSnake(newApplePosition)) {
+        newApplePosition = randomGridPosition();
+    };
+    return newApplePosition
+}
